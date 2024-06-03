00a88bf00151ae7d8cce7ab13e782b57be637cda
Przemek Dudek <przemod77@gmail.com>
2024-06-03 13:22:35 +0200

    chore: Refactor PersonUtils.setPin to accept String parameter

--- force-app/main/default/lwc/pin/pin.js
+++ force-app/main/default/lwc/pin/pin.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { LightningElement, api } from "lwc";
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
 
@@ -32,3 +33,35 @@ import { LightningElement } from 'lwc';
 
 export default class Pin extends LightningElement {}
 >>>>>>> a2691b9 (feat: Add Pin component and related files)
+=======
+import { LightningElement, api } from 'lwc';
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
+
+import setPin from '@salesforce/apex/PersonUtils.setPin';
+
+
+export default class Pin extends LightningElement {
+    @api recordId;
+    
+    handleGenerate() {
+        console.log('recordId', this.recordId);
+        setPin({ personId: this.recordId })
+            .then(result => {
+                const evt = new ShowToastEvent({
+                    title: "Success",
+                    message: "New Pin created",
+                    variant: "success",
+                    });
+                    this.dispatchEvent(evt);
+                })
+            .catch(error => {
+                const evt = new ShowToastEvent({
+                    title: "Error",
+                    message: "Something went wrong",
+                    variant: "error",
+                    });
+                    this.dispatchEvent(evt);
+                });
+    }
+}
+>>>>>>> fb9d9ed (chore: Refactor PersonUtils.setPin to accept String parameter)
