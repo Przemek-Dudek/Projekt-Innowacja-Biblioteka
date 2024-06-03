4be5309f0f601227eaca29cc0bddcc8d5cf6cae5
Przemek-Dudek <p.k.dudek@wp.pl>
2024-06-03 14:47:44 +0200

    fixed: Pin lwc

--- force-app/main/default/lwc/pin/pin.js
+++ force-app/main/default/lwc/pin/pin.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { LightningElement, api } from "lwc";
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
 
@@ -28,39 +26,3 @@ export default class Pin extends LightningElement {
                 });
     }
 }
-=======
-import { LightningElement } from 'lwc';
-
-export default class Pin extends LightningElement {}
->>>>>>> a2691b9 (feat: Add Pin component and related files)
-=======
-import { LightningElement, api } from 'lwc';
-import { ShowToastEvent } from "lightning/platformShowToastEvent";
-
-import setPin from '@salesforce/apex/PersonUtils.setPin';
-
-export default class Pin extends LightningElement {
-    @api recordId;
-    
-    handleGenerate() {
-        console.log('recordId', this.recordId);
-        setPin({ personId: this.recordId })
-            .then(result => {
-                const evt = new ShowToastEvent({
-                    title: "Success",
-                    message: "New Pin created",
-                    variant: "success",
-                    });
-                    this.dispatchEvent(evt);
-                })
-            .catch(error => {
-                const evt = new ShowToastEvent({
-                    title: "Error",
-                    message: "Something went wrong",
-                    variant: "error",
-                    });
-                    this.dispatchEvent(evt);
-                });
-    }
-}
->>>>>>> fb9d9ed (chore: Refactor PersonUtils.setPin to accept String parameter)
