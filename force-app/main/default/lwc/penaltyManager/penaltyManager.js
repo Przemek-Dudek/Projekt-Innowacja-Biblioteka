7dd09a8ad1c61fcd0fe9d086a53ef7c2f0a5bd47
Przemek Dudek <przemod77@gmail.com>
2024-06-10 12:41:56 +0200

    feat: Add removePenalty method to PersonUtils.cls and created penaltyManager LWC

---
+++ force-app/main/default/lwc/penaltyManager/penaltyManager.js
@@ -0,0 +1,28 @@
+import { LightningElement, api } from "lwc";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
+
+import setPin from '@salesforce/apex/PersonUtils.removePenalty';
+
+export default class PenaltyManager extends LightningElement {
+    @api recordId;
+    
+    handleRemovePenalty() {
+        removePenalty({ personId: this.recordId })
+            .then(result => {
+                const evt = new ShowToastEvent({
+                    title: "Success",
+                    message: "Penalty removed",
+                    variant: "success",
+                    });
+                    this.dispatchEvent(evt);
+                })
+            .catch(error => {
+                const evt = new ShowToastEvent({
+                    title: "Error",
+                    message: "Something went wrong",
+                    variant: "error",
+                    });
+                    this.dispatchEvent(evt);
+                });
+    }
+}
