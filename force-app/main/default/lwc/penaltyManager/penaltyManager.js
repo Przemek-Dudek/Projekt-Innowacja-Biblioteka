a7e8ff5333186bed5cd4c3f93f26bd755e8f223d
Przemek Dudek <przemod77@gmail.com>
2024-06-17 13:12:12 +0200

    refactor: Update removePenalty method in PersonUtils.cls and penaltyManager.js

--- force-app/main/default/lwc/penaltyManager/penaltyManager.js
+++ force-app/main/default/lwc/penaltyManager/penaltyManager.js
@@ -1,11 +1,27 @@
-import { LightningElement, api, wire } from "lwc";
+import { LightningElement, api, wire, track } from "lwc";
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
 
 import removePenalty from '@salesforce/apex/PersonUtils.removePenalty';
-import getRentals from '@salesforce/apex/PersonUtils.getRentals';
 
 export default class PenaltyManager extends LightningElement {
     @api recordId;
+
+    @track items = [
+        { id: 1, name: 'Książka 1', penalty: 10 },
+        { id: 2, name: 'Książka 2', penalty: 20 },
+        { id: 3, name: 'Książka 3', penalty: 0 },
+        { id: 4, name: 'Inna ksiazka', penalty: 30 },
+    ];
+
+    handleReset(event) {
+        const itemId = event.target.dataset.id;
+        this.items = this.items.map(item => {
+            if (item.id == itemId) {
+                return { ...item, penalty: 0 };
+            }
+            return item;
+        });
+    }
     
     handleRemovePenalty() {
         removePenalty({ personId: this.recordId })
@@ -27,15 +43,15 @@ export default class PenaltyManager extends LightningElement {
                 });
     }
 
-    @wire(getRentals, { personId: '$recordId' })
-        getRentals({ error, data }) {
-        if (data) {
-            this.rentals = data.map(rental => ({
-                label: rental.Id,
-                value: rental.Overdue_Payment__c
-            }));
-        } else if (error) {
-            console.log(error);
-        }
-    }
+    // @wire(getRentals, { personId: '$recordId' })
+    //     getRentals({ error, data }) {
+    //     if (data) {
+    //         this.rentals = data.map(rental => ({
+    //             label: rental.Id,
+    //             value: rental.Overdue_Payment__c
+    //         }));
+    //     } else if (error) {
+    //         console.log(error);
+    //     }
+    // }
 }
